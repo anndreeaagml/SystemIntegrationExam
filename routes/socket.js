1e78ec6d8bec7073a67a233077288725cda3c1f0
Andreea Vasiliu <58026816+anndreeaagml@users.noreply.github.com>
2022-11-29 23:52:16 +0100

    websocket

---
+++ routes/socket.js
@@ -0,0 +1,49 @@
+var express = require("express");
+const _ = require("lodash");
+const port=5000;
+const http = require('http');
+const app2=express();
+
+const server = app2.listen(port, () => {
+  console.log(`We are Listening on port ${port}...`);
+});
+
+const io = require('socket.io')(server, {
+    path: "/server"
+});
+
+let users = {};
+io.on('connection', (socket) => {
+
+  let userId = socket.handshake.query.userId; // GET USER ID
+  
+  // CHECK IS USER EXHIST 
+  if (!users[userId]) users[userId] = [];
+  
+  // PUSH SOCKET ID FOR PARTICULAR USER ID
+  users[userId].push(socket.id);
+   
+  // USER IS ONLINE BROAD CAST TO ALL CONNECTED USERS
+  io.sockets.emit("online", userId);
+  console.log(userId, "Is Online!", socket.id);
+
+  // DISCONNECT EVENT
+  socket.on('disconnect', (reason) => {
+
+    // REMOVE FROM SOCKET USERS
+    _.remove(users[userId], (u) => u === socket.id);
+    if (users[userId].length === 0) {
+      // ISER IS OFFLINE BROAD CAST TO ALL CONNECTED USERS
+      io.sockets.emit("offline", userId);
+      // REMOVE OBJECT
+      delete users[userId];
+    }
+   
+    socket.disconnect(); // DISCONNECT SOCKET
+
+    console.log(userId, "Is Offline!", socket.id);
+
+  });
+
+});
+module.exports = app2;
