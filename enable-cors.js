898445ad30f409548912fd52df15ab18b4d806fc
andreeavasiliu67 <andreea.vasiliu67@gmail.com>
2022-12-12 21:03:10 +0100

    without /

--- enable-cors.js
+++ enable-cors.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.use(methodOverride());
     var allowCrossDomain = function(req, res, next) {
         res.header('Access-Control-Allow-Credentials', true);
-        res.header('Access-Control-Allow-Origin', 'https://sgoatfrontend.azurewebsites.net/');
+        res.header('Access-Control-Allow-Origin', 'https://sgoatfrontend.azurewebsites.net');
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
